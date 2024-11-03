30b9a0dc4b289830299778298421dd76d5a70d61
Ivan Dubrov <dubrov.ivan@gmail.com>
2024-11-02 20:23:36 -0500

    Rewriting implementation to use timers
    Making I2C reads thread-safe. I2C reads cannot be invoked concurrently
    because of the way Klipper matches responses back to I2C read requests.
    If two requests are active at the same time, the first one to get
    a response will remove a response callback and, as a result, the next
    response causes Klipper to crash.
    
    Creating a timer handler to manage I2C reads and callbacks invocations.
    Making sure that we never execute two of these callbacks concurrently.

--- extras/eio_i2c_inputs.py
+++ extras/eio_i2c_inputs.py
@@ -4,6 +4,7 @@
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 import pins
+import logging
 from . import bus
 
 # Used to ignore acks from the buttons code
@@ -14,11 +15,13 @@ class EioI2cInputsSendAck():
 class EioI2cInputs(object):
     def __init__(self, config):
         self._printer = config.get_printer()
+        self._reactor = self._printer.get_reactor()
         name = config.get_name().split()[1]
         i2c = bus.MCU_I2C_from_config(config, default_speed=400000)
         mcu = i2c.get_mcu()
         mcu.register_config_callback(self._build_inputs_config)
-        # Input pins support
+
+        # Input pins handling
         self._init_cmds = []
         self._restart_cmds = []
         self._config_cmds = []
@@ -26,15 +29,30 @@ class EioI2cInputs(object):
         self._oid_to_pins = []
         self._oid_to_callbacks = []
         self._ack_count = 0
+        self._interrupt_update_timer = self._reactor.register_timer(self._interrupt_update_event)
+        self._last_interrupt_state = 0
+        self._last_pins = None
+        self._timer_running = False
 
         # Interrupt pin triggers button events
         interrupt_pin = config.get('interrupt_pin', None)
         if interrupt_pin is not None:
             buttons = self._printer.load_object(config, 'buttons')
-            buttons.register_button_push(interrupt_pin, self._send_buttons_state)
+            buttons.register_buttons([interrupt_pin], self._handle_interrupt)
         self._printer.register_event_handler("klippy:ready", self._ready)
     def _ready(self):
-        self._printer.get_reactor().register_async_callback(lambda e, s=self: s._send_buttons_state(e))
+        self._schedule_update()
+
+    def _handle_interrupt(self, eventtime, state):
+        self._last_interrupt_state = state
+        if state and not self._timer_running:
+            # Interrupt was triggered and timer is not currently running, schedule an update
+            # If we schedule a timer while it is running, it could be invoked by reactor
+            # concurrently with the existing execution. Concurrent I2C register reads cause
+            # cause Klipper to crash.
+            # Note: is it still possible to see "False" here, but timer is just about to return?
+            # Wouldn't that also cause the same issue?
+            self._schedule_update()
 
     def _build_inputs_config(self):
         # Build config commands
@@ -52,6 +70,7 @@ class EioI2cInputs(object):
             self._restart_cmds.append(cmd)
         else:
             self._config_cmds.append(cmd)
+
     def _interpret_init_cmd(self, cmd):
         parts = cmd.split()
         name = parts[0]
@@ -76,36 +95,64 @@ class EioI2cInputs(object):
             pass
         else:
             raise pins.error("Command is not supported by I2C inputs: %s" % name)
+
     def register_config_callback(self, cb):
         self._config_callbacks.append(cb)
+
     def create_oid(self):
         self._oid_to_pins.append([])
         self._oid_to_callbacks.append(None)
         return len(self._oid_to_pins) - 1
+
     def alloc_command_queue(self):
         return None
+
     def get_query_slot(self, oid):
         return 0
+
     def seconds_to_clock(self, time):
         return 0
+
     def register_response(self, callback, name, oid):
         if name != "buttons_state":
             raise pins.error("I2C inputs only supports buttons_state response callback")
         self._oid_to_callbacks[oid] = callback
+
     def lookup_command(self, msgformat, cq=None):
         parts = msgformat.split()
         name = parts[0]
         if name != "buttons_ack":
             raise pins.error("I2C inputs does not support '%s' command" % msgformat)
         return EioI2cInputsSendAck()
+
     def setup_input_pin(self, pin_idx, pullup):
         # Must be overridden to handle setup of the I2C input pin
         pass
+
     def read_input_pins(self):
         return 0
-    def _send_buttons_state(self, eventtime):
-        pins = self.read_input_pins()
 
+    def _schedule_update(self):
+        self._reactor.update_timer(self._interrupt_update_timer, self._reactor.NOW)
+
+    def _interrupt_update_event(self, eventtime):
+        self._timer_running = True
+        pins = self.read_input_pins()
+        logging.debug("I2C input pins values %d, previous value was %d." % (pins, self._last_pins))
+        if pins != self._last_pins:
+            self._invoke_buttons_callbacks(eventtime, pins)
+            self._last_pins = pins
+
+        # We keep re-reading input pins until interrupt pin detects that interrupt is reset
+        self._timer_running = False
+        if self._last_interrupt_state:
+            logging.debug("Interrupt button state is still '1' -- scheduling another update.")
+            return self._reactor.NOW
+        else:
+            logging.debug("Interrupt button state is reset to '0' -- pause the timer.")
+            return self._reactor.NEVER
+
+    def _invoke_buttons_callbacks(self, eventtime, pins):
         # Send updated buttons state through the callbacks
         self._ack_count = (self._ack_count + 1) & 0xff
         for oid, cb in enumerate(self._oid_to_callbacks):
@@ -120,4 +167,4 @@ class EioI2cInputs(object):
                     'state': [state],
                     '#receive_time': eventtime
                 }
-                self._printer.get_reactor().register_async_callback(lambda e, p=params, c=cb: c(p))
+                self._reactor.register_async_callback(lambda e, p=params, c=cb: c(p))
